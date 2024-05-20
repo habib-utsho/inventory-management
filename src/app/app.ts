10fbea49432a3346200b591a77097d0e817f10c5
Source-Code-007 <utsho926@gmail.com>
2024-05-21 03:19:30 +0600

    basic setup done

---
+++ src/app/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { orderRouter } from "../module/order/order.route";
+dotenv.config();
+const app:Application = express();
+
+// parser
+app.use(express.json())
+app.use(cors())
+
+
+// Router
+app.use('/api/v1/order', orderRouter)
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Success");
+});
+
+
+export default app
---
+++ src/app/server.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+  mongoose
+    .connect(`${process.env.MONGO_URI}` as string, {
+      //No More Deprecation Warning Options in Mongoose 6
+      //- these are no longer supported options in Mongoose 6 - only use it with old versions
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
+      //useCreateIndex: true,
+      //useFindAndModify: false
+    })
+    .then(() => {
+      console.log("Database connected!");
+    })
+    .catch((e) => {
+      console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
+      console.log("Something went wrong", e);
+    });
+});
+
