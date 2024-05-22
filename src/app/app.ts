7a8b39497e22d5de3b1deb2f5c79f385422456be
Source-Code-007 <utsho926@gmail.com>
2024-05-22 17:08:45 +0600

    not found error handler implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { orderRouter } from "../module/order/order.route";
 import { productRouter } from "../module/product/product.route";
+import { errorHandler, notFound } from "../middleware/errHandler";
 dotenv.config();
 const app:Application = express();
 
@@ -15,9 +16,15 @@ app.use(cors())
 app.use('/api/orders', orderRouter)
 app.use('/api/products', productRouter)
 
+
+// Demo homepage
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Success");
+  res.status(200).send("Homepage");
 });
+
+// Error handler middleware
+app.use(notFound);
+app.use(errorHandler);
 
 
 export default app
